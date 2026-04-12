ac7be468a46baa687e58db7e936bb8d3ef20be60
Cibi Chakravarthy <cchakravarthy@freewheel.com>
2026-04-12 19:05:10 +0530

    refactor: remove unnecessary comment from Step1Identity component layout

--- apps/web/components/onboarding/Step1Identity.tsx
+++ apps/web/components/onboarding/Step1Identity.tsx
@@ -297,7 +297,7 @@ export function Step1Identity({ onNext }: Props) {
   const initials = (fields.firstName?.[0] ?? '') + (fields.lastName?.[0] ?? '') || 'You';
 
   return (
-    <div className="space-y-6"> # sample
+    <div className="space-y-6">
       {/* Toast */}
       {toast && (
         <div
